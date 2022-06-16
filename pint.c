3676e4dee3b1d4387ab79f41616ed11699ebc2c5
mionki <piousmionki@gmail.com>
2022-06-16 18:52:48 +0300

    pint correction

--- pint.c
+++ pint.c
@@ -11,11 +11,9 @@ void m_pint(stack_t **stack, unsigned int line_number)
 {
 	stack_t *head = *stack;
 
-	if (var.stack_len == 0)
+	if (!head)
 	{
-		dprintf(STDOUT_FILENO,
-			"L%u: can't pint, stack empty\n",
-			line_number);
+		dprintf(STDERR_FILENO, "L%u: can't pint, stack empty\n" , line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", head->n);
