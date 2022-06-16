4818291731b5fb353012368733ed877268ce1878
akinyiv <akinyivee10@gmail.com>
2022-06-16 07:35:18 +0300

    push data

--- push.c
+++ push.c
@@ -1,51 +1,136 @@
 #include "monty.h"
-#include <ctype.h>
 
 /**
- * check_for_digit - checks that a string only contains digits
- * @arg: string to check
- *
- * Return: 0 if only digits, else 1
- */
-static int check_for_digit(char *arg)
+  * push - push data to top of stack.
+  * @stack: double pointer to stack.
+  * @line_number: number of lines.
+  *
+  * Return: void.
+  */
+void push(stack_t **stack, unsigned int line_number)
 {
-	int i;
+	stack_t *new_node = malloc(sizeof(stack_t));
 
-	for (i = 0; arg[i]; i++)
+	(void)line_number;
+	if (new_node != NULL)
 	{
-		if (arg[i] == '-' && i == 0)
-			continue;
-		if (isdigit(arg[i]) == 0)
-			return (1);
+	
+		new_node->prev = NULL;
+		new_node->next = NULL;
+
+		if (*stack == NULL)
+			*stack = new_node;
+		else
+		{
+			new_node->next = *stack;
+			(*stack)->prev = new_node;
+			*stack = new_node;
+		}
 	}
-	return (0);
+	else
+	{
+		free_dlist(*stack);
+		free(new_node);
+		err(4);
+	}
 }
 
 /**
- * m_push - push an integer onto the stack
- * @stack: double pointer to the beginning of the stack
- * @line_number: script line number
- *
- * Return: void
- */
-void m_push(stack_t **stack, unsigned int line_number)
+  * pop - removes the element at the top of the stack
+  * @stack: double pointer to stack.
+  * @line_number: number of lines.
+  *
+  * Return: void.
+  */
+void pop(stack_t **stack, unsigned int line_number)
 {
-	char *arg;
-	int n;
+	stack_t *temp;
 
-	arg = strtok(NULL, "\n\t\r ");
-	if (arg == NULL || check_for_digit(arg))
+	if (*stack != NULL && (*stack)->next == NULL)
 	{
-		dprintf(STDOUT_FILENO,
-			"L%u: usage: push integer\n",
-			line_number);
+		temp = *stack;
+		free(temp);
+		*stack = NULL;
+	}
+	else if (*stack != NULL && (*stack)->next != NULL)
+	{
+		temp = *stack;
+		(*stack)->next->prev = NULL;
+		*stack = (*stack)->next;
+		free(temp);
+	}
+	else
+	{
+		free_dlist(*stack);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	n = atoi(arg);
-	if (!add_node(stack, n))
+}
+
+/**
+  * swap - swaps the top two elements of the stack.
+  * @stack: double pointer to stack.
+  * @line_number: number of lines.
+  *
+  * Return: void.
+  */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	int temp = 0;
+
+	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		dprintf(STDOUT_FILENO, "Error: malloc failed\n");
+		free_dlist(*stack);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	var.stack_len++;
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
+}
+
+/**
+  * add - adds the top two elements of the stack.
+  * @stack: double pointer to stack;
+  * @line_number: number of lines.
+  *
+  * Return: void.
+  */
+void add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		free_dlist(*stack);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	*stack = (*stack)->next;
+	(*stack)->n = temp->n + temp->next->n;
+	free(temp);
+}
+
+/**
+  * sub - subtracts the top element of the stack from the second top element.
+  * @stack: double pointer to stack.
+  * @line_number: number of lines.
+  *
+  * Return: void.
+  */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		free_dlist(*stack);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = *stack;
+	*stack = (*stack)->next;
+	(*stack)->n = temp->next->n - temp->n;
+	free(temp);
 }
